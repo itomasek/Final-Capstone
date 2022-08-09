686ef04581b10291d29847a3fa7c26d06f54cae3
Kevin <kmeindl5@gmail.com>
2022-08-09 10:14:53 -0400

    Added some styling

---
+++ vue/src/components/Card.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div id="front" v-show="showFront">
+      <h1 class="subject">{{ card.subject }}</h1>
+      <p class="tags">{{ card.tags }}</p>
+      <h3 class="question">{{ card.question }}</h3>
+      <button class="edit">Edit</button>
+      <p class="author">{{ this.username }}</p>
+      <button class="flip">Flip</button>
+    </div>
+    <div id="back" v-show="!showFront">
+      <h1 class="subject">{{ card.subject }}</h1>
+      <p class="tags">{{ card.tags }}</p>
+      <h3 class="answer">{{ card.answer }}</h3>
+      <button class="edit">Edit</button>
+      <p class="author">{{ this.username }}</p>
+      <button class="flip">Flip</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "card",
+  props: ["card"],
+  computed: {
+    username() {
+      return this.$store.state.user.username;
+    },
+    data() {
+      return {
+        showFront: true,
+      };
+    },
+  },
+};
+</script>
+
+<style>
+</style>
--- vue/src/components/CardList.vue
+++ vue/src/components/CardList.vue
@@ -1,15 +1,19 @@
 <template>
-  <div>
-      <h1 v-for="card in cards" v-bind:key="card.cardId">{{card.question}}</h1>
+  <div class="cards-container">
+      <card v-for="card in cards" v-bind:key="card.cardId" v-bind:card="card"/>
   </div>
 </template>
 
 <script>
 
 import CardService from '../services/CardService.js';
+import Card from '../components/Card.vue';
 
 export default {
   name: "card-list",
+  components: {
+    Card
+  },
   data() {
     return {
       cards: [],
@@ -20,8 +24,13 @@ export default {
           this.cards = response.data;
       })
   }
+
 };
 </script>
 
 <style>
+.cards-container{
+  display: flex;
+  justify-content: space-around;
+}
 </style>
