982082bbd444b106238e543725f72d0a8ff8158e
Isabella Tomasek-Dryer <isabella.tomasek@gmail.com>
2022-08-12 13:16:41 -0400

    saving changes

--- vue/src/views/Information.vue
+++ vue/src/views/Information.vue
@@ -1,6 +1,60 @@
 <template>
-  <div>
-      <h1>About Us</h1>
+<div>
+<h1 class="aboutus">About Us</h1>
+  <div class="about">
+      <div class="name">
+        <img src="../../assets/notebook.png" alt="notebook">
+        <h3 class="nheader">Preston Masters</h3>
+        <div class="description">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A nisi laudantium aperiam unde recusandae vitae
+        perferendis ad sed? Pariatur reiciendis consectetur veritatis culpa rerum nisi ipsa similique qui illum
+        corporis.Lorem ipsum dolor sit amet consectetur adipisicing elit. A nisi laudantium aperiam unde recusandae vitae
+        perferendis ad sed? Pariatur reiciendis consectetur veritatis culpa rerum nisi ipsa similique qui illum
+        corporis.Lorem ipsum dolor sit amet consectetur adipisicing elit. A nisi laudantium aperiam unde recusandae vitae
+        perferendis ad sed? Pariatur reiciendis consectetur veritatis culpa rerum nisi ipsa similique qui illum
+        corporis.</p>
+        </div>
+      </div>
+      <div class="name">
+        <img src="../../assets/notebook.png" alt="notebook">
+        <h3 class="nheader">Ashar Bhatti</h3>
+        <div class="description">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A nisi laudantium aperiam unde recusandae vitae
+        perferendis ad sed? Pariatur reiciendis consectetur veritatis culpa rerum nisi ipsa similique qui illum
+        corporis.Lorem ipsum dolor sit amet consectetur adipisicing elit. A nisi laudantium aperiam unde recusandae vitae
+        perferendis ad sed? Pariatur reiciendis consectetur veritatis culpa rerum nisi ipsa similique qui illum
+        corporis.Lorem ipsum dolor sit amet consectetur adipisicing elit. A nisi laudantium aperiam unde recusandae vitae
+        perferendis ad sed? Pariatur reiciendis consectetur veritatis culpa rerum nisi ipsa similique qui illum
+        corporis.</p>
+        </div>
+      </div>
+      <div class="name">
+        <img src="../../assets/notebook.png" alt="notebook">
+        <h3 class="nheader">Kevin Meindl</h3>
+        <div class="description">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A nisi laudantium aperiam unde recusandae vitae
+        perferendis ad sed? Pariatur reiciendis consectetur veritatis culpa rerum nisi ipsa similique qui illum
+        corporis.Lorem ipsum dolor sit amet consectetur adipisicing elit. A nisi laudantium aperiam unde recusandae vitae
+        perferendis ad sed? Pariatur reiciendis consectetur veritatis culpa rerum nisi ipsa similique qui illum
+        corporis.Lorem ipsum dolor sit amet consectetur adipisicing elit. A nisi laudantium aperiam unde recusandae vitae
+        perferendis ad sed? Pariatur reiciendis consectetur veritatis culpa rerum nisi ipsa similique qui illum
+        corporis.</p>
+        </div>
+      </div>
+      <div class="name">
+        <img src="../../assets/notebook.png" alt="notebook">
+        <h3 class="nheader">Isabella Tomasek</h3>
+        <div class="description">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A nisi laudantium aperiam unde recusandae vitae
+        perferendis ad sed? Pariatur reiciendis consectetur veritatis culpa rerum nisi ipsa similique qui illum
+        corporis.Lorem ipsum dolor sit amet consectetur adipisicing elit. A nisi laudantium aperiam unde recusandae vitae
+        perferendis ad sed? Pariatur reiciendis consectetur veritatis culpa rerum nisi ipsa similique qui illum
+        corporis.Lorem ipsum dolor sit amet consectetur adipisicing elit. A nisi laudantium aperiam unde recusandae vitae
+        perferendis ad sed? Pariatur reiciendis consectetur veritatis culpa rerum nisi ipsa similique qui illum
+        corporis.</p>
+        </div>
+      </div>
+  </div>
   </div>
 </template>
 
@@ -11,5 +65,57 @@ export default {
 </script>
 
 <style>
+.about{
+  font-family:'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+  text-align: center;
+}
+.name{
+  margin-bottom: 20px;
+  padding: 20px;
+  border: solid 1px #555;
+}
+.nheader{
+  text-decoration: underline;
+  display: flex;
+  white-space:nowrap;
+}
+.aboutus{
+  text-decoration: underline;
+}
+div.name:nth-child(1){
+  display: flex;
+}
+div.name:nth-child(2){
+  display: flex;
+  flex-direction: row-reverse;
+}
+div.name:nth-child(3){
+  display: flex;
+}
+div.name:nth-child(4){
+  display: flex;
+  flex-direction: row-reverse;
+}
+div.name:nth-child(1)>div.description{
+  order: 2;
+}
+.about img{
+  width: 75px;
+  height: 75px;
+  border-radius: 999px;
+  margin: 0 10px;
+}
+.about:nth-child(1)>img{
+  order: 1;
+}
+.description{
+  padding:20px;
+    
+}
+.description>p{
+  text-align:left;
+}
+
+
 
 </style>
